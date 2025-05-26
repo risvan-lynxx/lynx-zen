d4cde7d91a6c2a30939a1fed06bb0965b54f2774
Lynx <risvanlynx777@gmail.com>
2025-05-26 18:28:33 +0530

    Create pp.js

---
+++ plugins/pp.js
@@ -0,0 +1,51 @@
+const plugins = require("../lib/event");
+const {
+    bot,
+    isPrivate,
+    clockString,
+    getUrl,
+    parsedJid,
+    isAdmin
+    
+} = require("../lib");
+const {
+    BOT_INFO
+} = require("../config");
+const config = require("../config");
+const { tiny } = require("../lib/fancy_font/fancy");
+const Jimp = require("jimp");
+const got = require("got");
+const fs = require("fs");
+
+bot(
+  {
+    pattern: "pp",
+    fromMe: true,
+    desc: "Set profile picture",
+    type: "user",
+  },
+  async (message, match, m) => {
+    if (!message.reply_message.image)
+      return await message.reply("_Reply to a photo_");
+    let buff = await m.quoted.download();
+    await message.setPP(message.user, buff);
+    return await message.reply("_Profile Picture Updated_");
+  }
+);
+/* Copyright (C) 2022 X-Electra.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+X-Asena - X-Electra
+*/
+bot(
+    {
+	pattern: 'setbio(.*)',
+	fromMe: true,
+	desc: 'to change your profile status',
+	type: 'user'
+}, async (message, match) => {
+	match = match || message.reply_message.text
+	if (!match) return await message.reply('*_Need Text_!*\n *Example: setbio _Zen-XD_*.')
+	await message.client.updateProfileStatus(match)
+	await message.reply('*_Successfully bio updated_*')
+})
