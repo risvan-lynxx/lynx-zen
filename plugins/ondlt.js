3d53430a45a6bb6f526e72952a23f152eea969b1
Lynx <risvanlynx777@gmail.com>
2025-05-26 18:36:13 +0530

    Create ondlt.js

---
+++ plugins/ondlt.js
@@ -0,0 +1,50 @@
+const plugins = require("../lib/event");
+const {
+    bot,
+    isPrivate,
+    clockString,
+    getUrl,
+    parsedJid,
+    isAdmin
+    
+} = require("../lib");
+const {
+    BOT_INFO
+} = require("../config");
+const config = require("../config");
+const { tiny } = require("../lib/fancy_font/fancy");
+const Jimp = require("jimp");
+const got = require("got");
+const fs = require("fs");
+
+bot(
+  {
+    pattern: "dlt",
+    fromMe: true,
+    desc: "deletes a message",
+    type: "misc",
+  },
+  async (message, match,m,client) => {
+    if (!message.reply_message) return await message.reply("*_Reply to a message_*"); {
+      await client.sendMessage(message.jid, { delete: message.reply_message.key })
+    }
+  }
+);
+/* Copyright (C) 2022 X-Electra.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+X-Asena - X-Electra
+*/
+bot(
+  {
+    pattern: "jid",
+    fromMe: true,
+    desc: "give",
+    type: "misc",
+  },
+  async (message, match) => {
+    return await message.sendMessage(
+      message.mention[0] || message.reply_message.jid || message.jid
+    );
+  }
+);
